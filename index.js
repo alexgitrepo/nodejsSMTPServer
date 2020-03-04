13078cb44a346d53f0bb329d468b1897b96ea069
alexgitrepo <alex.dev4web.com>
2020-03-04 23:31:30 +0500

    Add OAuth2

--- index.js
+++ index.js
@@ -9,14 +9,19 @@ app.use(bodyParser.json())
 const port = process.env.PORT
 const smtp_login = process.env.SMTP_LOGIN
 const smtp_password = process.env.SMTP_PASSWORD
+const refresh_token = process.env.REFRESH_TOKEN
+const client_id = process.env.CLINT_ID
+const client_secret = process.env.CLINT_ID
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    service: 'Gmail'
+    ,
     auth: {
+        type: 'OAuth2',
         user: smtp_login,
-        pass: smtp_password
+        refreshToken: refresh_token,
+        clientId: client_id,
+        clientSecret: client_secret,
     }
 })
 app.get('/', (req, res) => {
@@ -25,14 +30,15 @@ app.get('/', (req, res) => {
 
 app.post('/sendMessage', async (req, res) => {
     let {message, contacts, name} = req.body
-    try{let info = await transporter.sendMail({
-        from:'weblikeabc@gmail.com',
-        to: 'alex.dev4web@gmail.com',
-        subject: 'job offer',
-        html: `<b>Message from portfolio site</b><div>name:${name}</div><div>contacts:${contacts}</div><div>message:${message}</div>`
-    })
+    try {
+        let info = await transporter.sendMail({
+            from: 'weblikeabc@gmail.com',
+            to: 'alex.dev4web@gmail.com',
+            subject: 'job offer',
+            html: `<b>Message from portfolio site</b><div>name:${name}</div><div>contacts:${contacts}</div><div>message:${message}</div>`
+        })
         res.send(message, contacts, name)
-    }catch (e) {
+    } catch (e) {
         res.send(e)
     }
 
